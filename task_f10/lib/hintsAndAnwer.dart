af2f713bd0330e5b7df7406bb28477f9cefaa841
Roger Jay Sering <rogerjaysering@gmail.com>
2023-09-18 10:57:04 +0800

    finished task 10

---
+++ task_f10/lib/hintsAndAnwer.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:task_f10/main.dart';
+
+List<String> firstHints = [
+  'There was an accident',
+  'They use this to catch fish',
+  'He still has 1 yr left in contract',
+  'He is hailed as a king',
+  'He accomplished a mission',
+  'This is a very hot season',
+  'He has more points',
+  'He scored average 12 pts per game',
+  'They recruited him during olympics',
+  'Contract related',
+];
+
+List<String> secondHints = [
+  'His daughter did not survive',
+  'They use to to catch jellyfish in spongebob',
+  'He still has 1 yr left in contract hint 2',
+  'He is hailed as a king hint 2',
+  'He accomplished a mission hint 2',
+  'This is a very hot season hint 2',
+  'He has more points hint 2',
+  'He scored average 12 pts per game hint 2',
+  'They recruited him during olympics hint 2',
+  'Contract related hint 2',
+];
+
+List<String> questionAnswers = [
+  'Answer for number 1',
+  'Answer for number 2',
+  'Answer for number 3',
+  'Answer for number 4',
+  'Answer for number 5',
+  'Answer for number 6',
+  'Answer for number 7',
+  'Answer for number 8',
+  'Answer for number 9',
+  'Answer for number 10',
+];
+
+class HintsAnswers extends StatefulWidget {
+  const HintsAnswers(
+      {super.key, required this.questionClicked, required this.questionList});
+
+  final int questionClicked;
+  final List questionList;
+
+  @override
+  State<HintsAnswers> createState() => _HintsAnswersState();
+}
+
+class _HintsAnswersState extends State<HintsAnswers> {
+  int whatIndex = 0;
+  bool isClicked = false;
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            whatIndex = index;
+            counter++;
+            if(counter % 2 == 0)
+            {
+              isClicked = false;
+            }
+            else{
+              isClicked = true;
+            }
+          });
+        },
+        currentIndex: whatIndex,
+        selectedItemColor: Colors.blue,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.one_k), label: "Hint 1"),
+          BottomNavigationBarItem(icon: Icon(Icons.two_k), label: "Hint 2"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.question_answer), label: "The answer"),
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                widget.questionList[widget.questionClicked],
+                style: const TextStyle(fontSize: 20),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              IndexedStack(
+                index: whatIndex,
+                children: [
+                  AnimatedContainer(
+                    alignment: isClicked
+                        ? Alignment.centerLeft
+                        : Alignment.centerRight,
+                    duration: const Duration(seconds: 2),
+                    child: Text(
+                      firstHints[widget.questionClicked],
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  AnimatedContainer(
+                     alignment: isClicked
+                        ? Alignment.centerLeft
+                        : Alignment.centerRight,
+                    duration: const Duration(seconds: 2),
+                    child: Text(
+                      secondHints[widget.questionClicked],
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  AnimatedContainer(
+                     alignment: isClicked
+                        ? Alignment.centerLeft
+                        : Alignment.centerRight,
+                    duration: const Duration(seconds: 2),
+                    child: Text(
+                      questionAnswers[widget.questionClicked],
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ task_f10/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:task_f10/hintsAndAnwer.dart';
+
+List<String> questions = [
+  'Is Kobe Still Alive?',
+  'What team does Kevin Durant play?',
+  'Is Damian Lilard still playing?',
+  'Who holds as the best scorer in the league?',
+  'Why did Lebron leave Cleveland Cavs?',
+  'When is the NBA summer league?',
+  'Is Michael Jordan better than Kobe?',
+  'Is Russel Westbrook still in Lakers?',
+  'Is Dwayne Wayde retired?',
+  'When will Lebron stop playing?',
+];
+void main() {
+  runApp(const QuestionsPanel());
+}
+
+class QuestionsPanel extends StatefulWidget {
+  const QuestionsPanel({super.key});
+
+  @override
+  State<QuestionsPanel> createState() => _QuestionsPanelState();
+}
+
+class _QuestionsPanelState extends State<QuestionsPanel> {
+  Column displayCols() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 0; i < questions.length; i++)
+          Builder(
+            builder: (context) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return HintsAnswers(
+                          questionClicked: i,
+                          questionList: questions,
+                        );
+                      },
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    questions[i],
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ),
+              );
+            }
+          ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: displayCols(),
+        ),
+      ),
+    );
+  }
+}
